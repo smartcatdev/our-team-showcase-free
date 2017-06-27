85e89cccfcc3ebbdc5e12b53b7c6fb5ab1e9b6a8
Eric Green <eric@smartcat.ca>
2017-06-27 12:09:47 -0400

    Delete plugin data on uninstall

--- uninstall.php
+++ uninstall.php
@@ -6,5 +6,37 @@ if( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     die;
 }
 
-include_once dirname( __FILE__ ) . 'constants.php';
+include_once dirname( __FILE__ ) . '/constants.php';
+include_once dirname( __FILE__ ) . '/includes/custom-post-type.php';
 
+
+// Trash all team member posts
+$posts = get_posts( array(
+    'post_type'      => 'team_member',
+    'posts_per_page' => -1
+) );
+
+foreach( $posts as $post ) {
+    wp_trash_post( $post->ID );
+}
+
+
+// Delete all terms from the taxonomy
+register_team_member_position_taxonomy();
+
+$terms = get_terms( array(
+    'taxonomy'   => 'team_member_position',
+    'hide_empty' => false,
+) );
+
+foreach( $terms as $term ) {
+    wp_delete_term( $term->term_id, 'team_member_position' );
+}
+
+
+// Delete all plugin settings
+$options = new \ReflectionClass( Options::class );
+
+foreach( $options->getConstants() as $option ) {
+    delete_option( $option );
+}
