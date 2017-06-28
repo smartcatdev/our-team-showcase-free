8e2524ec54688232e2d21dd4fb5c73d4e6843537
Eric Green <eric@smartcat.ca>
2017-06-28 12:38:53 -0400

    Switch to a generic template

--- templates/default.php
+++ templates/default.php
@@ -7,66 +7,63 @@ $members = get_members_in_order();
 ?>
 
 <?php //TODO fix css files ?>
-<div id="sc_our_team" class="grid <?php esc_attr_e( ' sc-col' . get_option( Options::GRID_COLUMNS ) ); ?>">
 
-    <?php if ( $members->have_posts() ) : ?>
+<?php if ( $members->have_posts() ) : ?>
 
-        <?php while ( $members->have_posts() ) : $members->the_post(); ?>
+    <?php while ( $members->have_posts() ) : $members->the_post(); ?>
 
-            <div itemscope itemtype="http://schema.org/Person" class="sc_team_member">
+        <div itemscope itemtype="http://schema.org/Person" class="sc_team_member">
 
-                <div class="sc_team_member_inner">
+            <div class="sc_team_member_inner">
 
-                    <img src="<?php echo esc_url( get_member_avatar( get_post() ) ); ?>" />
+                <img src="<?php echo esc_url( get_member_avatar( get_post() ) ); ?>"/>
 
-                    <?php if( get_option( Options::DISPLAY_NAME ) == 'on' ) : ?>
+                <?php if ( get_option( Options::DISPLAY_NAME ) == 'on' ) : ?>
 
-                        <div itemprop="name" class="sc_team_member_name"><?php the_title() ?></div>
+                    <div itemprop="name" class="sc_team_member_name"><?php the_title() ?></div>
 
-                    <?php endif; ?>
+                <?php endif; ?>
 
-                    <?php if( get_option( Options::DISPLAY_TITLE ) == 'on' ) : ?>
+                <?php if ( get_option( Options::DISPLAY_TITLE ) == 'on' ) : ?>
 
-                        <div itemprop="jobtitle" class="sc_team_member_jobtitle">
-                            <?php echo get_post_meta( get_the_ID(), 'team_member_title', true ); ?>
-                        </div>
+                    <div itemprop="jobtitle" class="sc_team_member_jobtitle">
+                        <?php echo get_post_meta( get_the_ID(), 'team_member_title', true ); ?>
+                    </div>
 
-                    <?php endif; ?>
+                <?php endif; ?>
 
-                    <div class="sc_team_content"><?php the_content(); ?></div>
+                <div class="sc_team_content"><?php the_content(); ?></div>
 
-                    <?php if( get_option( Options::SHOW_SOCIAL ) == 'on' ) : ?>
+                <?php if ( get_option( Options::SHOW_SOCIAL ) == 'on' ) : ?>
 
-                        <div class="icons"><?php do_member_social_links( get_post() ); ?></div>
+                    <div class="icons"><?php do_member_social_links( get_post() ); ?></div>
 
-                    <?php endif; ?>
+                <?php endif; ?>
 
-                    <div class="sc_team_member_overlay"></div>
+                <div class="sc_team_member_overlay"></div>
 
-                    <?php if( $get_attr( 'single_template' ) !== 'disabled' ) : ?>
+                <?php if ( $get_attr( 'single_template' ) !== 'disabled' ) : ?>
 
-                        <div class="sc_team_more">
-                            <a href="<?php the_permalink() ?>" rel="bookmark">
-                                <img src="<?php echo esc_url( asset( 'images/more.png' ) ); ?>"/>
-                            </a>
-                        </div>
+                    <div class="sc_team_more">
+                        <a href="<?php the_permalink() ?>" rel="bookmark">
+                            <img src="<?php echo esc_url( asset( 'images/more.png' ) ); ?>"/>
+                        </a>
+                    </div>
 
-                    <?php endif; ?>
-
-                </div>
+                <?php endif; ?>
 
             </div>
 
-            <?php wp_reset_postdata(); ?>
+        </div>
 
-        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
 
-    <?php else : ?>
+    <?php endwhile; ?>
 
-        <p><?php _e( 'No team members have been added yet', 'ots' ); ?></p>
+<?php else : ?>
 
-    <?php endif; ?>
+    <p><?php _e( 'No team members have been added yet', 'ots' ); ?></p>
 
-    <div class="clear"></div>
+<?php endif; ?>
 
-</div>
+<div class="clear"></div>
--- templates/grid-circles.php
+++ templates/grid-circles.php
@@ -2,71 +2,11 @@
 
 namespace ots;
 
-$members = get_members_in_order();
-
 ?>
 
 <?php //TODO fix css files ?>
 <div id="sc_our_team" class="grid <?php esc_attr_e( ' sc-col' . get_option( Options::GRID_COLUMNS ) ); ?>">
 
-    <?php if ( $members->have_posts() ) : ?>
-
-        <?php while ( $members->have_posts() ) : $members->the_post(); ?>
-
-            <div itemscope itemtype="http://schema.org/Person" class="sc_team_member">
-
-                <div class="sc_team_member_inner">
-
-                    <img src="<?php echo esc_url( get_member_avatar( get_post() ) ); ?>" />
-
-                    <?php if( get_option( Options::DISPLAY_NAME ) == 'on' ) : ?>
-
-                        <div itemprop="name" class="sc_team_member_name"><?php the_title() ?></div>
-
-                    <?php endif; ?>
-
-                    <?php if( get_option( Options::DISPLAY_TITLE ) == 'on' ) : ?>
-
-                        <div itemprop="jobtitle" class="sc_team_member_jobtitle">
-                            <?php echo get_post_meta( get_the_ID(), 'team_member_title', true ); ?>
-                        </div>
-
-                    <?php endif; ?>
-
-                    <div class="sc_team_content"><?php the_content(); ?></div>
-
-                    <?php if( get_option( Options::SHOW_SOCIAL ) == 'on' ) : ?>
-
-                        <div class="icons"><?php do_member_social_links( get_post() ); ?></div>
-
-                    <?php endif; ?>
-
-                    <div class="sc_team_member_overlay"></div>
-
-                    <?php if( $get_attr( 'single_template' ) !== 'disabled' ) : ?>
-
-                        <div class="sc_team_more">
-                            <a href="<?php the_permalink() ?>" rel="bookmark">
-                                <img src="<?php echo esc_url( asset( 'images/more.png' ) ); ?>"/>
-                            </a>
-                        </div>
-
-                    <?php endif; ?>
-
-                </div>
-
-            </div>
-
-            <?php wp_reset_postdata(); ?>
-
-        <?php endwhile; ?>
-
-    <?php else : ?>
-
-        <p><?php _e( 'No team members have been added yet', 'ots' ); ?></p>
-
-    <?php endif; ?>
-
-    <div class="clear"></div>
+    <?php include_once dirname( __FILE__ ) . '/default.php'; ?>
 
 </div>
