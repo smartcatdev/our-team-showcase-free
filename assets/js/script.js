0f6eb6483816104f453e622ddc723d6eff1cb7a5
Eric Green <ericgreen.developer@gmail.com>
2017-07-27 13:56:31 -0400

    Fix glitch on twentysixteen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,39 @@
-jQuery( document ).ready( function( $ ){
+jQuery(document).ready(function ($) {
 
     do_resize();
 
-    $( window ).resize( function() {
+    $(window).resize(function () {
         do_resize();
 
     });
 
     function do_resize() {
 
-        var members = $( '#sc_our_team.grid .sc_team_member, ' +
-                         '#sc_our_team.grid_circles .sc_team_member,' +
-                         '#sc_our_team.grid_circles2 .sc_team_member');
+        var members = $('#sc_our_team.grid .sc_team_member, ' +
+            '#sc_our_team.grid_circles .sc_team_member,' +
+            '#sc_our_team.grid_circles2 .sc_team_member');
 
         var member_height = members.width();
 
-        members.each( function( index, el ){
-            $( el ).find( '.sc_team_member_inner' ).css( { height: member_height } );
-        } );
+        members.each(function (index, el) {
+            $(el).find('.sc_team_member_inner').css({height: member_height});
+        });
 
 
-        members.mouseenter( function() {
+        members.mouseenter(function () {
 
-            $( this ).find( '.sc_team_member_overlay' ).stop( true, false ).fadeIn( 440 );
-            $( this ).find( '.wp-post-image' ).addClass( 'zoomIn' );
-            $( this ).find( '.sc_team_more' ).addClass( 'show' );
+            $(this).find('.sc_team_member_overlay').stop(true, false).fadeIn(440);
+            $(this).find('.wp-post-image').addClass('zoomIn');
+            $(this).find('.sc_team_more').addClass('show');
 
-        } ).mouseleave( function() {
+        }).mouseleave(function () {
 
-            $( this ).find( '.sc_team_member_overlay' ).stop( true, false ).fadeOut( 440 );
-            $( this ).find( '.wp-post-image' ).removeClass( 'zoomIn' );
-            $( this ).find( '.sc_team_more' ).removeClass( 'show' );
+            $(this).find('.sc_team_member_overlay').stop(true, false).fadeOut(440);
+            $(this).find('.wp-post-image').removeClass('zoomIn');
+            $(this).find('.sc_team_more').removeClass('show');
 
-        } );
+        });
 
     }
 
-} );
+});
