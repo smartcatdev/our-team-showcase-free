65cd0c55d5869275afc63c85b0fdc113605081a9
bilalhassan <contact@bilalhassan.com>
2017-08-12 01:44:56 -0400

    cleanup desc

--- our-team-showcase.php
+++ our-team-showcase.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Our Team Showcase
- * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+ * Plugin URI: https://smartcatdesign.net/downloads/our-team-showcase/
  * Description: Display your team members in a very attractive way as a widget or page with the shortcode [our-team]
  * Version: 4.0.0
  * Author: Smartcat
