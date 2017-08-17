141dbe4f35bee56cd3798322410aa44e42f94a9a
Mahshad <mopomoor@gmail.com>
2017-08-17 18:16:08 +0430

    Fixed a bug
    Fixed a bug that caused error: " Parse error: syntax error, unexpected '=', expecting ')' "

--- includes/functions.php
+++ includes/functions.php
@@ -227,8 +227,9 @@ function get_member_articles( $member = null ) {
         $article = get_post_meta( $member->ID, "team_member_article$ctr", true );
 
         if( !empty( $article ) ) {
+		$post = get_post( $article );
 
-        	if ( !empty( $post = get_post( $article ) ) ) {
+        	if ( !empty( $post ) ) {
 		        $articles[] = $post;
 	        }
 
